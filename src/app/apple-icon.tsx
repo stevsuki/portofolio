48f413acfeecf4e8f5c105dbe845925d11684c44
stevensuki <steven.suki@ifabula.com>
2026-07-30 13:59:31 +0700

    [Steven] - adjust metadata image ss

---
+++ src/app/apple-icon.tsx
@@ -0,0 +1,32 @@
+import { ImageResponse } from "next/og";
+
+export const size = {
+	width: 180,
+	height: 180,
+};
+export const contentType = "image/png";
+
+export default function AppleIcon() {
+	return new ImageResponse(
+		(
+			<div
+				style={{
+					width: "100%",
+					height: "100%",
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+					background: "white",
+					fontSize: 112,
+					fontWeight: 700,
+					letterSpacing: -5,
+					color: "#0d9488",
+					fontFamily: "system-ui, sans-serif",
+				}}
+			>
+				SS
+			</div>
+		),
+		{ ...size }
+	);
+}
